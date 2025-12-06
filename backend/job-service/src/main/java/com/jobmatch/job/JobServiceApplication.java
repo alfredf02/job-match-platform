9b98aac94b181ac51d5c1b8e41218ab6a8e6bb7e
Alfred <alfredfrancis124@gmail.com>
2025-12-06 19:19:26 +1100

    added job-service

--- backend/job-service/src/main/java/com/jobmatch/job/JobServiceApplication.java
+++ backend/job-service/src/main/java/com/jobmatch/job/JobServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.jobmatch.job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend/job-service/src/main/java/com/jobmatch/job/repository/JobRepository.java
@@ -0,0 +1,7 @@
+package com.jobmatch.job.repository;
+
+import com.jobmatch.job.domain.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+}
