64d38ba33f9f250b331edc5fa21ed9a528a4a0c0
Alfred <alfredfrancis124@gmail.com>
2025-12-06 19:34:28 +1100

    Revert "added job-service"
    This reverts commit 9b98aac94b181ac51d5c1b8e41218ab6a8e6bb7e.

--- backend/job-service/src/main/java/com/example/demo/JobServiceApplication.java
+++ backend/job-service/src/main/java/com/example/demo/JobServiceApplication.java
@@ -1,4 +1,4 @@
-package com.jobmatch.job;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/job-service/src/main/java/com/jobmatch/job/repository/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jobmatch.job.repository;
-
-import com.jobmatch.job.domain.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
